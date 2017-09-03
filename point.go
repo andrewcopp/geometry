709a70199bbde1c9232a2b57d7605494388bce07
Andrew Copp <andrew.w.copp@gmail.com>
2017-09-03 01:03:24 -0400

    Created a Function to Expand Package

--- point.go
+++ point.go
@@ -1,5 +1,7 @@
 package geometry
 
+import "math"
+
 // Point represents a one-dimensional space in a Cartesian plane.
 type Point struct {
 	X int
@@ -12,4 +14,11 @@ func NewPoint(x, y int) *Point {
 		X: x,
 		Y: y,
 	}
+}
+
+// To returns the distance to another point.
+func (p *Point) To(other *Point) float64 {
+	X2 := math.Pow(float64(p.X-other.X), 2)
+	Y2 := math.Pow(float64(p.Y-other.Y), 2)
+	return math.Sqrt(X2 + Y2)
 }
